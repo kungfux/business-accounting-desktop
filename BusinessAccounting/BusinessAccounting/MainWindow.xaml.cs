c746a8ba810298ea555a5949ca30d6dc755dcdd8
Alexander Fuks <kungfux2010@gmail.com>
2014-11-25 01:09:52 +0300

    App with Metro is created

--- BusinessAccounting/BusinessAccounting/MainWindow.xaml.cs
+++ BusinessAccounting/BusinessAccounting/MainWindow.xaml.cs
@@ -12,13 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace BusinessAccounting
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {

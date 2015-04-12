3d0484b14ae01c8293de6bea1ec1aca18934485a
Alexander Fuks <kungfux2010@gmail.com>
2015-04-12 04:00:07 +0300

    Phase 2. ORM. ORM is configured and checked.

---
+++ BusinessAccounting/BusinessAccounting/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using BusinessAccounting.Domain;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessAccounting
+{
+    public partial class App
+    {
+        public App()
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(CashOperation).Assembly);
+
+            new SchemaExport(cfg).Execute(false, true, false);
+        }
+    }
+}
---
+++ BusinessAccounting/BusinessAccounting/Domain/CashOperation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusinessAccounting.Domain
+{
+    class CashOperation
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Sum { get; set; }
+        public string Comment { get; set; }
+    }
+}

e7c15358a5d756832d8c0060864dbee213973bff
Alexander Fuks <kungfux2010@gmail.com>
2015-04-15 16:15:10 +0300

    Add new MainWindow window New UI will be implemented on new window Added ability to store/restore window position

---
+++ BusinessAccounting/BusinessAccounting/Views/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BusinessAccounting.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // Store WindowPosition setting
+            Properties.Settings.Default.WindowPosition = this.RestoreBounds;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Restore WindowPosition setting
+            try
+            {
+                Rect bounds = Properties.Settings.Default.WindowPosition;
+                this.Top = bounds.Top;
+                this.Left = bounds.Left;
+                if (this.SizeToContent == SizeToContent.Manual)
+                {
+                    this.Width = bounds.Width;
+                    this.Height = bounds.Height;
+                }
+            }
+            catch
+            {
+                Debug.WriteLine("No WindowPosition setting stored.");
+            }
+        }
+    }
+}

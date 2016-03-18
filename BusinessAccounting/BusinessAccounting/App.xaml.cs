377e3e292543f3d2abea321a1726fd261e20af29
Alexander Fuks <kungfux2010@gmail.com>
2016-03-18 21:57:50 +0300

    Previous implemetation is removed

--- BusinessAccounting/BusinessAccounting/App.xaml.cs
+++ BusinessAccounting/BusinessAccounting/App.xaml.cs
@@ -1,13 +1,8 @@
 ﻿using System.Windows;
-using Xclass.Database;
 
 namespace BusinessAccounting
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-        public static SQLite3Query sqlite = new SQLite3Query();
     }
 }
--- BusinessAccounting/BusinessAccounting/Properties/AssemblyInfo.cs
+++ BusinessAccounting/BusinessAccounting/Properties/AssemblyInfo.cs
@@ -1,55 +1,17 @@
 ﻿using System.Reflection;
 using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Windows;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Business Accounting")]
+[assembly: AssemblyTitle("Business Accounting Universal")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Fuks Alexander")]
+[assembly: AssemblyCompany("Alexander Fuks")]
 [assembly: AssemblyProduct("Business Accounting")]
-[assembly: AssemblyCopyright("Copyright © Fuks Alexander 2014")]
+[assembly: AssemblyCopyright("Copyright © Alexander Fuks 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: ComVisible(true)]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-//In order to begin building localizable applications, set 
-//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-//inside a <PropertyGroup>.  For example, if you are using US english
-//in your source files, set the <UICulture> to en-US.  Then uncomment
-//the NeutralResourceLanguage attribute below.  Update the "en-US" in
-//the line below to match the UICulture setting in the project file.
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-    //(used if a resource is not found in the page, 
-    // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-    //(used if a resource is not found in the page, 
-    // app, or any theme specific resource dictionaries)
-)]
-
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]

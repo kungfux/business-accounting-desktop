da9bfed0dcfcad6b33273896c7a1e69c0ec6594d
Alexander Fuks <kungfux2010@gmail.com>
2016-03-22 04:13:07 +0300

    Save/Restore window bounds are added

--- BusinessAccounting/BusinessAccounting/View/MainWindow.xaml.cs
+++ BusinessAccounting/BusinessAccounting/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessAccountingControls;
+using System.Windows;
 
 namespace BusinessAccounting.View
 {
@@ -10,6 +11,27 @@ namespace BusinessAccounting.View
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Top = Properties.Settings.Default.Top;
+            this.Left = Properties.Settings.Default.Left;
+            this.Height = Properties.Settings.Default.Height;
+            this.Width = Properties.Settings.Default.Width;
+            
+            if (Properties.Settings.Default.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void BusinessAccountingWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.Top = RestoreBounds.Top;
+            Properties.Settings.Default.Left = RestoreBounds.Left;
+            Properties.Settings.Default.Height = RestoreBounds.Height;
+            Properties.Settings.Default.Width = RestoreBounds.Width;
+            Properties.Settings.Default.WindowState = this.WindowState;
+
+            Properties.Settings.Default.Save();
         }
     }
 }

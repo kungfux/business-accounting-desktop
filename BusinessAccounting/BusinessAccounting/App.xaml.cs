0f427a0671b0907bfb0fdcb508ae5b7fb1868857
Alexander Fuks <kungfux2010@gmail.com>
2016-03-22 04:44:54 +0300

    Multilanguages support is added

--- BusinessAccounting/BusinessAccounting/App.xaml.cs
+++ BusinessAccounting/BusinessAccounting/App.xaml.cs
@@ -1,8 +1,32 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading;
+using System.Windows;
 
 namespace BusinessAccounting
 {
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            SetLanguageDictionary();
+        }
+
+        private void SetLanguageDictionary()
+        {
+            ResourceDictionary dict = new ResourceDictionary();
+            switch (Thread.CurrentThread.CurrentCulture.ToString())
+            {
+                case "en-US":
+                    dict.Source = new Uri("Resources\\StringResources.en-US.xaml", UriKind.Relative);
+                    break;
+                case "ru-RU":
+                    dict.Source = new Uri("Resources\\StringResources.ru-RU.xaml", UriKind.Relative);
+                    break;
+                default:
+                    dict.Source = new Uri("Resources\\StringResources.en-US.xaml", UriKind.Relative);
+                    break;
+            }
+            this.Resources.MergedDictionaries.Add(dict);
+        }
     }
 }

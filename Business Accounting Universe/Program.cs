02f7c45eaf586b7fd11a9da81b443e5d440b9ce3
Alexander Fuks <Alexander.V.Fuks@gmail.com>
2017-02-17 21:50:34 +0300

    Add mutex

--- Business Accounting Universe/Program.cs
+++ Business Accounting Universe/Program.cs
@@ -1,17 +1,34 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 using BusinessAccountingUniverse.View;
+using MetroFramework;
 
 namespace BusinessAccountingUniverse
 {
-    public class Program
+    internal class Program
     {
+        private const string MutexId = "d0b1994d-0a5d-4cd6-864f-7346fc81cc1f";
+
         [STAThread]
-        public static void Main()
+        internal static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            bool singleApp;
+            using (new Mutex(true, MutexId, out singleApp))
+            {
+                if (!singleApp)
+                {
+                    MessageBox.Show($"The program is already running.{Environment.NewLine}" + 
+                        "Running two or more simultaneous sessions is not recommended.",
+                        "Business Accounting Universe", 
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
         }
     }
 }

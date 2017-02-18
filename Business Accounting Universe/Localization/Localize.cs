f8933610fd949c51b1e2380ebd45f8605fbab298
Alexander Fuks <kungfux2010@gmail.com>
2017-02-18 13:44:05 +0300

    Add exceptions catch

--- Business Accounting Universe/Localization/Localize.cs
+++ Business Accounting Universe/Localization/Localize.cs
@@ -1,5 +1,4 @@
 ﻿using System.Data;
-using System.IO;
 using System.Reflection;
 using System.Windows.Forms;
 
--- Business Accounting Universe/Program.cs
+++ Business Accounting Universe/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
 using System.Threading;
 using System.Windows.Forms;
 using BusinessAccountingUniverse.Localization;
@@ -11,6 +13,7 @@ namespace BusinessAccountingUniverse
     {
         private static readonly Logger Log = LogManager.GetCurrentClassLogger();
         private const string MutexId = "d0b1994d-0a5d-4cd6-864f-7346fc81cc1f";
+        private const string GithubIssueUrl = "https://github.com/kungfux/business-accounting/issues/new";
 
         private static readonly Localize Localize = new Localize();
 
@@ -30,6 +33,8 @@ namespace BusinessAccountingUniverse
                 }
 
                 Application.ThreadException += OnThreadException;
+                Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+                AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
@@ -37,9 +42,43 @@ namespace BusinessAccountingUniverse
             }
         }
 
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            LogError(e.ExceptionObject);
+        }
+
         private static void OnThreadException(object sender, ThreadExceptionEventArgs t)
         {
-            Log.Fatal(t);
+            LogError(t.Exception);
+        }
+
+        private static void LogError(object data)
+        {
+            Log.Fatal(data);
+
+            if (MessageBox.Show(string.Format(Localize.GetStringById("UnhandledException"), Environment.NewLine), 
+                Localize.ApplicationName,MessageBoxButtons.YesNo, MessageBoxIcon.Error) != DialogResult.Yes)
+            {
+                return;
+            }
+
+            try
+            {
+                Clipboard.SetText($"App name: {Localize.ApplicationName}{Environment.NewLine}" +
+                    $"Version: {Assembly.GetEntryAssembly().GetName().Version}{Environment.NewLine}" +
+                    $"Problem: {data}");
+
+                MessageBox.Show(string.Format(Localize.GetStringById("DetailsCopiedToClipboard"), Environment.NewLine),
+                    Localize.ApplicationName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Process.Start(GithubIssueUrl);
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Unable to open GItHub to submit an issue: {e.Message}");
+            }
+
+            Application.Exit();
         }
     }
 }
